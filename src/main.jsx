405e2dfe52fac587876eab339e3db3b29bb9faa7
Fedal-leonard <leonardfedal@gmail.com>
2024-07-01 10:31:03 +0200

    add existing project to git

---
+++ src/main.jsx
@@ -0,0 +1,5 @@
+import ReactDom from "react-dom/client";
+import App from "./app.jsx";
+import "./index.css";
+
+ReactDom.createRoot(document.getElementById("root")).render(<App />);
---
+++ src/pages/Home.jsx
@@ -0,0 +1,50 @@
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import Loader from "../components/Loader";
+import Island from "../models/Island";
+{
+  /* <div
+        className="absolute top-28 left-0 right-0 z-10 flex
+      items-center justify-center"
+      >
+        POPUP
+      </div> */
+}
+const Home = () => {
+  const adjustIslanForScreenSize = () => {
+    let screenScale = null;
+    let screenPosition = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+    return [screenScale, screenPosition, rotation];
+  };
+  const [islandScale, islandPosition, islandrotation] =
+    adjustIslanForScreenSize();
+  return (
+    <section className="w-full h-screen relative">
+      <Canvas
+        className="w-full h-screen bg-transparent"
+        camera={{ near: 0.1, far: 1000 }}
+      >
+        <Suspense fallback={<Loader />}>
+          <directionalLight />
+          <ambientLight />
+          <pointLight />
+          <spotLight />
+          <hemisphereLight />
+          <Island
+            scale={islandScale}
+            position={islandPosition}
+            rotation={islandrotation}
+          />
+        </Suspense>
+      </Canvas>
+    </section>
+  );
+};
+
+export default Home;
